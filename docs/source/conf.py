e9eb5331df0c827b2d5dc56cc8fe5f29f682a1c6
Bane Sullivan <banesulli@gmail.com>
2020-01-07 20:01:23 -0700

    Fix spinx-gallery issue on read the docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -82,7 +82,7 @@ sphinx_gallery_conf = {
     # Sort gallery example by file name instead of number of lines (default)
     "within_subsection_order": FileNameSortKey,
     # directory where function granular galleries are stored
-    "backreferences_dir": False,
+    "backreferences_dir": None,
     # Modules for which function level galleries are created.  In
     "doc_module": "taco_vis",
     "image_scrapers": ('matplotlib'),
